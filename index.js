f2cbb8b29a4f798a77339bc1614b73cf7e95dc50
wesbdss <wesleybenicio4@gmail.com>
2021-04-06 21:11:09 -0300

    a

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 var app = express();
-
+const cors = require('cors')
 var port = process.env.PORT || 4000
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
+app.use(cors())
 app.use('/assets', express.static(__dirname + '/assets'));
 
 app.get('/', (req,res,next)=> {
